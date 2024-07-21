db31f8b4dfd16e9bcd63965082cd97167f992af2
Bo1x <alejandroboixperez@gmail.com>
2024-07-21 15:56:19 +0200

    paralax done

---
+++ UnityProject/Assets/Scripts/MainMenu/Parallax.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public float offsetMultiplier = 1f;
+    public float smoothTime = .3f;
+
+    private Vector2 startPosition;
+    private Vector3 velocity;
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector2 offset = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        if (offset != null && offset.y != float.NaN && offset.x != float.NaN)
+        {
+
+           transform.position = Vector3.SmoothDamp(transform.position, startPosition + (offset * offsetMultiplier), ref velocity, smoothTime);
+        }
+    }
+}
